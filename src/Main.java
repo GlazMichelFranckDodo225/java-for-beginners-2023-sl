9c23d02ef60f18f7a85fa39a01fa107e44c99e83
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2023-12-16 12:20:56 +0100

    Ask for the User Input with the "Scanner" Utility Class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello and welcome!");
+        Scanner scanner = new Scanner(System.in);
+
+        // Ask for the User Input
+        System.out.println("Enter a Number : ");
+        // Storing User Input
+        int intNumber = scanner.nextInt();
+        // Displaying the Number
+        if((intNumber % 2) == 0) {
+            System.out.println("The Number is Even");
+        } else {
+            System.out.println("The Number is Odd");
+        }
     }
 }
